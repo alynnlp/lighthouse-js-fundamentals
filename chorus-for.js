a31ce632d723762f413c805c9007a05a2ce96c15
Aileen Pang <aileenpang63@gmail.com>
2017-08-24 13:28:00 -0400

    Adds looping exercises

---
+++ chorus-for.js
@@ -0,0 +1,8 @@
+var chorus = "Let's dance!";
+for (var repeat = 0; repeat < 10; repeat++) {
+  /**the first most commonly to declare our counter variable,
+  the second for defining our while condition,
+  and the last for updating the state of our loop after every step.**/
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
